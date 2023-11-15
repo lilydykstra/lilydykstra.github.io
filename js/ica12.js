61dcf54a906bce4cfaa15821f0a75257f9061a2a
lilydykstra <lidy4526@colorado.edu>
2023-11-14 18:47:16 -0700

    update ica 12

--- js/ica12.js
+++ js/ica12.js
@@ -1,6 +1,6 @@
 const newBtn = document.querySelector('#js-new-quote').addEventListener('click',getQuote);
 const questionText = document.querySelector('#js-quote-text')
-const answerBtn = document.querySelector('#js-tweet').addEventListener('click', empty);
+const answerBtn = document.querySelector('#js-tweet').addEventListener('click', displayAnswer);
 let answer = '';
 
 const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
@@ -27,12 +27,16 @@ async function getQuote() {
 
 function displayQuote(question) {
     const questionTxt = document.querySelector('#js-quote-text');
+    const answerTxt = document.querySelector('#js-answer-text'); 
     questionTxt.textContent = question;
+    answerTxt.textContent = '';
+
+
 }
 
-function displayAnswer(answer) {
+function displayAnswer() {
     const answerTxt = document.querySelector('#js-answer-text');
-    answerTxt.textContent = '';
+    answerTxt.textContent = answer;
 }
 
 getQuote();
