3ff6c3cc19017e99aa607252cdff0190e093b53a
lilydykstra <lidy4526@colorado.edu>
2023-12-12 15:31:14 -0700

    js updates

--- atls2200/js-midterm/js-midterm.js
+++ atls2200/js-midterm/js-midterm.js
@@ -1,22 +1,40 @@
 // there are 10 boxes in the middle of the screen
 // they all start with a zero in them
 // when clicked they go up a digit uptil 9 when it resets to 0
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const boxes = document.querySelectorAll('.box');
+  let animationInterval;
 
+  // resets all numbers to 0
+  const clearNumbers = () => {
+    boxes.forEach(box => {
+      box.textContent = '0';
+      box.style.transform = 'none'; // Reset the transform property
+    });
+  };
+
+  // Function to show popup
+  const submitScreen = () => {
+    alert('Thanks for submitting you number! Please never come back :)');
+  };
+
+  // use "click" event listener for clear button
+  document.getElementById('clearButton').addEventListener('click', clearNumbers);
+
+  // use "click" event listener for submit button
+  document.getElementById('submitButton').addEventListener('click', submitScreen);
+
+  // use "click" event listener for each box to change value to random number
   boxes.forEach((box, index) => {
-    box.addEventListener('click', function() {
-      // Generate a random number between 0 and 9
+    let value = Math.floor(Math.random() * 10);
+  
+    box.addEventListener('click', function () {
       const randomNumber = Math.floor(Math.random() * 10);
       box.textContent = randomNumber;
+
     });
   });
 });
 
 
-// if clear button gets clicked
-//   all number reset to 0
 
-// if submit button is clicked
-//   popup with "you submited you number"
