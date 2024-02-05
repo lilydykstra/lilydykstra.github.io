ddcd087923b00fc6cb2b8e29771ecad394691068
lilydykstra <lidy4526@colorado.edu>
2024-02-04 22:31:42 -0700

    design updates

--- lilywebsite/index.js
+++ lilywebsite/index.js
@@ -1,8 +1,11 @@
-// Add this to your existing JavaScript file or create a new one
-const carouselContainer = document.querySelector('.carousel-container');
+document.addEventListener("DOMContentLoaded", function() {
+    var images = document.querySelectorAll('img');
 
-// Set up event listeners for smooth scrolling
-carouselContainer.addEventListener('wheel', (e) => {
-    e.preventDefault();
-    carouselContainer.scrollLeft += e.deltaY;
+    var colors = ['#f8d3fe', '#98e6cd', '#adc8fe', '#fee2a1', '#cebefe']; // Array of 5 different color choices
+
+    images.forEach(function(image) {
+        var randomColorIndex = Math.floor(Math.random() * colors.length); // Generate a random index to pick a color
+        var randomColor = colors[randomColorIndex]; // Get the random color from the array
+        image.style.border = '5px solid ' + randomColor; // Apply the random color as the border
+    });
 });
